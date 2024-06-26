49eaac004bc73cd0b9a7a22030abf2c871c67d8a
1ustaa <sergey00rus24680@gmail.com>
2024-06-26 16:05:06 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,48 @@
+import dash
+from dash import dcc, html
+import dash_tabulator
+import pandas as pd
+
+
+csv_file = 'cost_item_dic.csv'
+
+df = pd.read_csv(csv_file)
+
+df['parent'] = df['parent'].replace({pd.NA: None, float('nan'): None})
+
+data = [{'id': row['id'],
+         'parent': row['parent'],
+         'name': row['name'],
+         'id_1c': row['id_1c'],
+         'children': []} for index, row in df.iterrows()]
+
+id_map = {item['id_1c']: item for item in data}
+
+for item in data:
+    parent_id = item['parent']
+    if parent_id:
+        parent_item = id_map.get(parent_id)
+        if parent_item:
+            parent_item['children'].append(item)
+
+parent_data = [item for item in data if item['parent'] is None]
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dash_tabulator.DashTabulator(
+        id='table',
+        columns=[
+            {'title': 'ID', 'field': 'id'},
+            {'title': 'Name', 'field': 'name'},
+        ],
+        data=parent_data,
+        options={
+            "dataTree": True,
+            "dataTreeChildField": "children"
+        }
+    ),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
